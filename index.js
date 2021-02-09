67ba892cc22d6ad159675752daafb0dd00b915ad
Ugin-gt <apach575@gmail.com>
2021-02-09 16:33:01 +0200

    Add react simple class example

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // React;
-// ReactDom;
+// ReactDOM;
 
-const root = document.getElementById('root');
+class Heading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    }
+
+render() {
+  const { title, children } = this.props; // пропсы  настройка компонента извне
+  return React.createElement(
+    'h1',
+    { title: title, tabIndex: 0, className: 'heading' },
+    'Hello React.js',
+    ...children
+  );
+}
+}
 
 const reactElement = React.createElement(
-  'h1',
-  { title: 'Hello', tabIndex: 0, className:'heading' },
-  'Hello React.js'
-  );
+  Heading,
+  { title: 'Hello  heading' },
+  ' string 1',
+  ' string 2',
 
-ReactDOM.render(reactElement, root);
+);
+
+ReactDOM.render(reactElement, document.getElementById('root'));
